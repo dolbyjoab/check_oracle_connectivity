d9b267a2cc16de9964db5ffaa52cdd187fee55d0
Etienne Delsy <24556128+dolbyjoab@users.noreply.github.com>
2019-05-08 07:03:17 -0700

    Add secret part

--- check_oracle_connectivity.sh
+++ check_oracle_connectivity.sh
@@ -12,31 +12,46 @@
 
 ###########################################################################################
 
-
-
-############# 
-## Variables
-#############
+export FILE=/opt/sensu/plugins/ctm/.secret-settings.json
 ECHO='/usr/bin/echo'
-SQLPLUS64='/usr/bin/sqlplus64'
-GREP='/usr/bin/grep'
-USER=$1
-PASS=$2
-DB=$3
-
 #######################################
 ### Verifying the parameters' number
 #######################################
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 2 ]; then
         $ECHO "Illegal number of parameters"
-        $ECHO "USAGE: $0 user password database"
+        $ECHO "USAGE: $0 database nameOfSecret"
         exit 2
 fi
+
+
+#############
+## Variables
+#############
+SQLPLUS64='/usr/bin/sqlplus64'
+GREP='/usr/bin/grep'
+CAT='/usr/bin/cat'
+DB=$1
+SECRET=$2
+APIKEY=$($CAT $FILE |jq '.["api-key"]'| cut -d "\"" -f 2)
+API_ID=$($CAT $FILE |jq '.["api-id"]'| cut -d "\"" -f 2)
+ENDPOINT=$($CAT $FILE |jq '.["endpoint"]'| cut -d "\"" -f 2)
+#Get the credentials
+PASS_TEST=$(curl -s -X POST https://$ENDPOINT -H "x-apigw-api-id:$API_ID" -H "x-api-key:$APIKEY" -d '{"secret": '\"$SECRET\"' }'|grep "not found")
+        if [ $? -eq 0 ]
+                then
+                $ECHO "UNKNOWN - The secret $SECRET was not found; Check it with monitoring team!"
+                exit 3
+        fi
+
+
+USERNAME=$(curl -s -X POST https://$ENDPOINT -H "x-apigw-api-id:$API_ID" -H "x-api-key:$APIKEY" -d '{"secret": '\"$SECRET\"' }' 2> /dev/null|jq '.["username"]'| cut -d "\"" -f 2  )
+PASSWORD=$(curl -s -X POST https://$ENDPOINT -H "x-apigw-api-id:$API_ID" -H "x-api-key:$APIKEY" -d '{"secret": '\"$SECRET\"' }' 2> /dev/null |jq '.["password"]'| cut -d "\"" -f 2 )
+
 
 #############################################################
 ## try connection and look for string "connected"
 ############################################################
-$ECHO "exit" | $SQLPLUS64 -L $USER/$PASS@$DB |$GREP "Connected" -A1 > /dev/null
+$ECHO "exit" | $SQLPLUS64 -L $USERNAME/$PASSWORD@$DB |$GREP "Connected" -A1 > /dev/null
 
         if [ $? -eq 0 ]
                 then
@@ -45,5 +60,5 @@ $ECHO "exit" | $SQLPLUS64 -L $USER/$PASS@$DB |$GREP "Connected" -A1 > /dev/null
         elif [ $? -eq 1 ]
                 then
                 $ECHO "CRITICAL - We could not connect to $DB"
-                exit 1
+                exit 2
         fi
